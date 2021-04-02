c4dc89447362178b621bed715c41c598ee758da4
Jailton Junior <jailton.junior94@outlook.com>
2021-04-02 19:44:56 -0300

    Ajustes na URL de Produção

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
-// import axios from 'axios';
-
-// export const api = axios.create({
-//     baseURL: 'https://financialcontrol-api.herokuapp.com',
-// });
-
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://financialcontrol-api.herokuapp.com',
 });
+
+// import axios from 'axios';
+
+// export const api = axios.create({
+//     baseURL: 'http://localhost:3000',
+// });
