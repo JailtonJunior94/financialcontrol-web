4590782f0fb9b8212ccacbefb56f669ac874abf0
Jailton Junior <jailton.junior94@outlook.com>
2021-04-16 22:20:24 -0300

    Criação do gráfico de contas da casa

---
+++ src/models/bills.ts
@@ -0,0 +1,8 @@
+export interface Bill {
+    id: string;
+    date: Date;
+    total: number;
+    sixtyPercent: number;
+    fortyPercent: number;
+    active: boolean;
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -10,17 +10,25 @@ import {
 
 import { api } from '../../services/api';
 import { Layout } from '../../components/Layout';
+import { Bill } from '../../models/bills';
 import { Transaction } from '../../models/transactions';
 import { formatDateA, formatMoney } from '../../utils/formats';
 
 export function Dashboard() {
     const [isLoading, setIsLoading] = useState(true);
     const [transactions, setTransactions] = useState<Transaction[]>([])
-    const labels = transactions.map(j => formatDateA(j.date));
+    const [bills, setBills] = useState<Bill[]>([])
+    const labelTransactions = transactions.map(j => formatDateA(j.date));
+    const labelBills = bills.map(j => formatDateA(j.date));
 
     async function loadTransactions() {
-        const response = await api.get<Transaction[]>('api/v1/transactions');
-        setTransactions(response.data);
+        const [transactions, bills] = await Promise.all([
+            api.get<Transaction[]>('api/v1/transactions'),
+            api.get<Bill[]>('api/v1/bills'),
+        ])
+
+        setTransactions(transactions.data);
+        setBills(bills.data);
         setIsLoading(false);
     }
 
@@ -62,7 +70,7 @@ export function Dashboard() {
                                     data: transactions.map(t => t.outcome)
                                 }
                             ]}
-                            labels={labels}
+                            labels={labelTransactions}
                             options={{
                                 tooltips: {
                                     enabled: true,
@@ -75,27 +83,27 @@ export function Dashboard() {
                     </CCardBody>
                 </CCard>
                 <CCard>
-                    <CCardHeader>Transações Mensais</CCardHeader>
+                    <CCardHeader>Contas da Casa</CCardHeader>
                     <CCardBody>
                         <CChartBar
                             datasets={[
                                 {
                                     label: 'Total',
-                                    backgroundColor: '#363f5f',
-                                    data: transactions.map(t => t.total)
+                                    backgroundColor: '#A9A9A9',
+                                    data: bills.map(b => b.total)
                                 },
                                 {
-                                    label: 'Entradas',
-                                    backgroundColor: '#33CC95',
-                                    data: transactions.map(t => t.income)
+                                    label: '60 %',
+                                    backgroundColor: '#0000FF',
+                                    data: bills.map(b => b.sixtyPercent)
                                 },
                                 {
-                                    label: 'Saídas',
-                                    backgroundColor: '#e52e4d',
-                                    data: transactions.map(t => t.outcome)
+                                    label: '40 %',
+                                    backgroundColor: '#00FFFF',
+                                    data: bills.map(b => b.fortyPercent)
                                 }
                             ]}
-                            labels={labels}
+                            labels={labelBills}
                             options={{
                                 tooltips: {
                                     enabled: true,
